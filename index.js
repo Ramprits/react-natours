14b139944221c9b059499cfe3b84ad227b70157e
Ramprits <rampritsahani@outlook.in>
2019-08-24 11:31:20 +0530

    4 files modified

--- index.js
+++ index.js
@@ -1,26 +1,72 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
+const App = () => {
+  const [counte, setCounter] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    getPosts(search);
+  }, [query])
+
+  const getPosts = async (search) => {
+    if (search !== "") {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts?title=${search}`)
+      const data = await res.json();
+      setPosts(data)
+    }
+
   }
 
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
+  const updateSerach = e => {
+    setSearch(e.target.value)
   }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setQuery(search)
+
+  }
+
+
+
+  return (
+    <div className="box">
+      <form onSubmit={onSubmit}>
+
+        <div className="field">
+          <p className="control">
+            <input className="input"
+              value={search} name="search" type="text" placeholder="Enter your serach" onChange={updateSerach} />
+          </p>
+        </div>
+
+        <div className="field">
+          <p className="control">
+            <button className="button is-success">
+              Search
+    </button>
+          </p>
+        </div>
+      </form>
+      <br />
+      <div className="box">
+        <ui>
+          {posts.map((item) => {
+            return (
+              <li key={item.id}>
+                {item.title}
+              </li>
+            )
+          })}
+        </ui>
+
+      </div>
+    </div>
+  );
 }
 
 render(<App />, document.getElementById('root'));
